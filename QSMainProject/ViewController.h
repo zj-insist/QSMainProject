3d4f6467aebabd4c773e2919f97e27e6b48dc6bc
ZJ-Jie <zj_lostself@163.com>
2017-08-24 15:06:53 +0800

    :beers: 创建项目

---
+++ QSMainProject/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  QSMainProject
+//
+//  Created by ZJ-Jie on 2017/8/24.
+//  Copyright © 2017年 Jie. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ QSMainProject/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  QSMainProject
+//
+//  Created by ZJ-Jie on 2017/8/24.
+//  Copyright © 2017年 Jie. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
